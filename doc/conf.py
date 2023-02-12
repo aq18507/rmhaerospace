065710ff64ff09fada1fb6ef08ab018648df43a5
Rafael Heeb <rafael.heeb@bristol.ac.uk>
2023-02-12 16:50:21 +0000

    update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -26,6 +26,17 @@ project = "RHM Aerospace"
 copyright = f'{year}, RMH Aerospace'
 author = 'Rafael Heeb'
 
+#-- Source -----------------------------------------------------------
+# 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
 # -- General configuration ---------------------------------------------------
 #
 
